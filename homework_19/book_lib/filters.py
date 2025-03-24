09ad303464356402ad17d6494063ef606d9d1191
dmytro.m <dmytro@hoofdfabriek.nl>
2025-03-24 22:07:01 +0100

    Homework N19. [fix after review

--- homework_19/book_lib/filters.py
+++ homework_19/book_lib/filters.py
@@ -1,11 +1,17 @@
 import django_filters
+from django_filters import CharFilter
+
 from .models import Book
 
 
 class BookFilter(django_filters.FilterSet):
-    author = django_filters.CharFilter(field_name="author", lookup_expr="icontains")
-    genres = django_filters.CharFilter(field_name="genres", lookup_expr="icontains")
+    """
+    Filter class for Book model.
+    Allows filtering by author and genres using case-insensitive search.
+    """
+    author: CharFilter = django_filters.CharFilter(field_name="author", lookup_expr="icontains")
+    genres: CharFilter = django_filters.CharFilter(field_name="genres", lookup_expr="icontains")
 
     class Meta:
-        model = Book
-        fields: list = ['author', 'genres']
+        model: type[Book] = Book
+        fields: list[str] = ["author", "genres"]
--- homework_19/book_lib/models.py
+++ homework_19/book_lib/models.py
@@ -1,17 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import CharField, DateTimeField, ForeignKey
+
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    genre = models.CharField(max_length=100)
-    publication_year = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    """
+    Represents a book with details such as title, author, genre, publication year, and metadata.
+
+    This model holds information about books for an application. Used to store
+    and retrieve book records. It also specifies metadata for default ordering.
+
+    :ivar title: The title of the book.
+    :type title: str
+    :ivar author: The author of the book.
+    :type author: str
+    :ivar genre: The genre of the book.
+    :type genre: str
+    :ivar publication_year: The publication year of the book.
+    :type publication_year: int
+    :ivar created_at: The date and time of the record creation.
+    :type created_at: datetime.datetime
+    :ivar user: The user associated with the book. References a user instance.
+    :type user: User
+    """
+    title: CharField = models.CharField(max_length=255)
+    author: CharField = models.CharField(max_length=255)
+    genre: CharField = models.CharField(max_length=100)
+    publication_year: CharField = models.PositiveIntegerField()
+    created_at: DateTimeField = models.DateTimeField(auto_now_add=True)
+    user: ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering: list = ['publication_year', 'title']
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
