6795ce8dd4eaf2cf5fd9a7ed895f10ae0825ca02
dmytro.m <dmytro@hoofdfabriek.nl>
2025-05-15 13:21:36 +0200

    fix homework 15 . fix model

--- homework_15/main/models.py
+++ homework_15/main/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 
 class Post(models.Model):
+    """
+    Модель поста с заголовком, содержимым и датой создания.
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
+        """
+        Возвращает строковое представление поста с заголовком и ID.
+        """
         return f"{self.title} (ID: {self.id})"
